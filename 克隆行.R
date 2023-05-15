d9a1c772f2d5dc86d37334cc15739b6207983b2f
liwenyu127 <116156758+liwenyu127@users.noreply.github.com>
2023-05-15 18:43:18 +0800

    Create 克隆行.R

---
+++ 克隆行.R
@@ -0,0 +1,30 @@
+clonerow<-function(data,n,x,y)
+{data<-data.frame(data)
+l=length(data)
+data<-cbind(data,data.frame(x),data.frame(y))
+mydata=data.frame()
+i=1
+while(i<l)
+{
+  data1<-data[i,]#逐行获取数据
+  j=1
+  print(j)
+  while(j<=n+1)
+  {
+  if(j==1){data1$x="N"
+  data1$y=0}
+  else{data1$x="Y"
+  data1$y=j-1}
+  j=j+1
+  mydata<-rbind(mydata,data1)}
+  
+  i=i+1
+}
+return(mydata)
+}
+
+table = data.frame(
+  姓名 = c("张三", "李四","王五"),
+  工号 = c("001","002","003"),
+  月薪 = c(1000, 2000,3000)
+)
